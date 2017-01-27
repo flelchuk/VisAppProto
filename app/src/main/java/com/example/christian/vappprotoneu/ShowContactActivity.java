c8a011ab7264305adbf28ffdbb933ffb8301dd5d
Scorpion008 <christian@holberg-scheepers.de>
2017-01-27 13:44:33 +0100

    Code für Export des Kontaktes eingefuegt

--- app/src/main/java/com/example/christian/vappprotoneu/ShowContactActivity.java
+++ app/src/main/java/com/example/christian/vappprotoneu/ShowContactActivity.java
@@ -3,12 +3,163 @@ package com.example.christian.vappprotoneu;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.Manifest;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 public class ShowContactActivity extends AppCompatActivity {
 
+    private void exportContact(String vcardstr) {
+
+        //TODO: String parsen, vermutlich an anderer Stelle sinnvoller
+
+        final int REQUEST_PERMISSION_WRITE_CONTACTS=1;
+
+        String DisplayName  = "Max Mustermann";
+        String MobileNumber = "0172112233";
+        String HomeNumber   = "0202445566";
+        String WorkNumber   = "0202778899";
+        String emailID      = "mustermann@musterisp.de";
+        String company      = "Musterfirma AG";
+        String jobTitle     = "CEO";
+        String Address      = "Musterstrasse 12";
+
+        //Wenn APILevel < 23, dann Rechte bei Installation erhalten,
+        //sonst muss waehrend der  Laufzeit angefordert werden
+
+        //Rechte zum Schreiben von Kontakten nicht vorhanden?
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //Dann Rechte anfragen
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_CONTACTS},
+                    REQUEST_PERMISSION_WRITE_CONTACTS);
+        }
+        if (DisplayName != null && !DisplayName.equals(""))
+        {
+            //Liste an auszufuehrenden Operationen anlegen
+            ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+
+            //Kontakt anlegen
+            ops.add(ContentProviderOperation.newInsert(
+                    ContactsContract.RawContacts.CONTENT_URI)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                    .build());
+
+            //Name
+            ops.add(ContentProviderOperation.newInsert(
+                    ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(
+                            ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                            DisplayName).build());
+
+            //Nummer Handy
+            if (MobileNumber != null && !MobileNumber.equals("")) {
+                ops.add(ContentProviderOperation.
+                        newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, MobileNumber)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                        .build());
+            }
+
+            //Nummer Festnetz
+            if (HomeNumber != null && !HomeNumber.equals("")) {
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, HomeNumber)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
+                        .build());
+            }
+
+            //Nummer Arbeit
+            if (WorkNumber != null && !WorkNumber.equals("")) {
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, WorkNumber)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                ContactsContract.CommonDataKinds.Phone.TYPE_WORK)
+                        .build());
+            }
+
+            //EMail
+            if (emailID != null && emailID.equals("")) {
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Email.DATA, emailID)
+                        .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                        .build());
+            }
+
+            //Firma
+            if (!company.equals("") && !jobTitle.equals("")) {
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, company)
+                        .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+                        .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, jobTitle)
+                        .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+                        .build());
+            }
+
+            //Adresse
+            if (Address != null && !Address.equals(""))
+            {
+                ops.add(ContentProviderOperation.newInsert(
+                        ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                        .withValue(
+                                ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS,
+                                Address).build());
+            }
+
+            // Liste mit Befehlen an Contact Provider ubergeben
+            try {
+                getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+                Toast.makeText(getApplicationContext(), "Kontakt " + DisplayName + " exportiert", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Toast.makeText(getApplicationContext(), "Fehler: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Fehler", Toast.LENGTH_SHORT).show();
+                //Log.i("EXE",e.getMessage());
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
